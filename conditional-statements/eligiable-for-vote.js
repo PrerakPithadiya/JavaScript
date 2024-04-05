e07df042e9f95f974ef6cfbe65a6e211f904ef2d
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-04-05 23:03:01 +0530

    Add files via upload

---
+++ conditional-statements/eligiable-for-vote.js
@@ -0,0 +1,7 @@
+let age = prompt("enter your age: ");
+
+if (age >= 18) {
+    console.log("you are eligible for vote");
+} else {
+    console.log("you aren't eligible for vote");
+}
--- conditional-statements/even-or-odd.js
+++ conditional-statements/even-or-odd.js
@@ -1,1 +1,7 @@
+let num = prompt("enter the number: ");
 
+if (num % 2 == 0) {
+    console.log(`the given number ${num} is even`);
+} else {
+    console.log(`the given number ${num} is odd`);
+}
