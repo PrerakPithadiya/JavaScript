3d0c5d1bd3a3bd2e25991612885c572cdb5737c5
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-04-05 23:25:46 +0530

    Add files via upload

--- patterns/right-half-pyramid.js
+++ patterns/right-half-pyramid.js
@@ -1,1 +1,10 @@
+let n = prompt("enter the number of rows: ");
+let str;
 
+for (let i = 1; i <= n; i++) {
+    str = "";
+	for (let j = 1; j <= i; j++) {
+		str = str + `* `;
+	}
+	console.log(str);
+}
