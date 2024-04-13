e425936c40aa27749f9d34f565ee951e5d422065
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-04-13 10:41:00 +0530

    Add files via upload

--- strings/10 - escape-characters.js
+++ strings/10 - escape-characters.js
@@ -1,1 +1,21 @@
+// Using escape characters in JavaScript
 
+// Escape double quotes within a string
+var message1 = "She said, \"Hello!\"";
+console.log(message1);
+
+// Escape single quotes within a string
+var message2 = 'He said, \'How are you?\'';
+console.log(message2);
+
+// Insert a newline character
+var message3 = "First line\nSecond line";
+console.log(message3);
+
+// Insert a tab character
+var message4 = "Name:\tJohn";
+console.log(message4);
+
+// Insert a backslash character
+var message5 = "This is a backslash: \\";
+console.log(message5);
